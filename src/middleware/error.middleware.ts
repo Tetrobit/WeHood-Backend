57297d28e08ae426aededa3c0a599445dd5da6fe
russian_proger <sagitov.2806@mail.ru>
2025-05-22 22:56:12 +0300

    fix: rename unused parameter

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-export const errorMiddleware = (err: Error, req: Request, res: Response, _next: NextFunction) => {
+export const errorMiddleware = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
     console.error(err.stack);
     return res.status(500).json({
         message: "Internal Server Error",
