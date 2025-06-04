6d1484b938d34907ea9a9903d2cef1794faa5b9c
russian_proger <sagitov.2806@mail.ru>
2025-06-05 00:43:05 +0300

    remove logging

--- src/services/upload.service.ts
+++ src/services/upload.service.ts
@@ -9,7 +9,6 @@ export async function uploadFile(uri: string): Promise<null | {
     mimeType: string;
 }> {
     const formData = new FormData();
-    console.log(uri);
     const bytes = fs.readFileSync(uri);
     const mimeType = mime.lookup(uri);
     const blob = new Blob([bytes], { type: mimeType });
